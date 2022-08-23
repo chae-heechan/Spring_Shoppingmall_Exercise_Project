1566a51e714b06638c970c160ffdbd324c78ee50
Chae hee chan <eqholic1125@gmail.com>
2022-08-23 19:00:15 +0900

    setting

---
+++ src/main/java/chae/Spring_Shoppingmall_Exercise_Project/SpringShoppingmallExerciseProjectApplication.java
@@ -0,0 +1,13 @@
+package chae.Spring_Shoppingmall_Exercise_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShoppingmallExerciseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShoppingmallExerciseProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chae/Spring_Shoppingmall_Exercise_Project/SpringShoppingmallExerciseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package chae.Spring_Shoppingmall_Exercise_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShoppingmallExerciseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
